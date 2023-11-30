ea6a4b46f619db811ab79617c99257ef61f278a2
Keiter0309 <vuongtuankiet33@gmail.com>
2023-11-30 16:03:53 +0700

    update commit

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -1,12 +1,19 @@
 <template>
-<!--Start Sidebar-->
-    <sidebar/>
-<!--End Sidebar-->
+    <!--Start Sidebar-->
+        <sidebar/>
+    <!--End Sidebar-->
+
+    <!--Start Content-->
+        <content/>
+    <!--End Content-->
+
+
 </template>
 
 <script setup>
 
 import Sidebar from "./sidebar.vue";
+import Content from "./content.vue";
 </script>
 
 <style scoped>
